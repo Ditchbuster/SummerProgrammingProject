62600a6ac09b7d2a55138f0e1d6252cd2b87dec9
Chris P <trioman008@hotmail.com>
2012-03-30 15:10:20 -0600

    Uploading Projects Base

---
+++ GameCore/src/GameCore.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GameCore.cpp
+// Author      : Chris P
+// Version     :
+// Copyright   : GNU
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
