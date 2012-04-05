26f8ce0e6944735943518638831b02024a60c94b
Chris P <trioman008@hotmail.com>
2012-04-05 14:09:47 -0600

    Adding Game Client Java project

---
+++ GameClientJava/src/gameclient/GameClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gameclient;
+
+/**
+ * @author Chris
+ *
+ */
+public class GameClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
